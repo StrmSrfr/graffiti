ec36c9bd3bfc57eb3766fd0cebebf1a532b1e759
Samuel Edwin Ward <seward@cytochro.me>
2013-10-13 12:46:16 -0400

    hello world!

--- graffiti.lisp
+++ graffiti.lisp
@@ -5,3 +5,22 @@
 (defvar *graffiti* nil
   "List of current graffiti")
 
+(hunchentoot:define-easy-handler (index :uri "/") ()
+  (setf (hunchentoot:content-type*) "text/html")
+  (cl-who:with-html-output-to-string (s)
+    (:html
+     (:head
+      (:title "Graffiti 2.0: New and improved!")
+      )
+     (:body
+      (:h1 "Hello whirled!")))))
+
+(defvar *acceptor* nil
+  "Hunchentoot acceptor.")
+
+(defvar *port* 57587
+  "Port to listen on.")
+
+(defun start ()
+  (setf *acceptor*
+        (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port *port*))))
