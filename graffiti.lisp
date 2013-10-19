a17d1222a8a1655eb82c911da155846a389f70e2
Samuel Edwin Ward <seward@cytochro.me>
2013-10-19 13:35:25 -0400

    display Graffiti from memory

--- graffiti.lisp
+++ graffiti.lisp
@@ -13,7 +13,15 @@
       (:title "Graffiti 2.0: New and improved!")
       )
      (:body
-      (:h1 "Hello whirled!")))))
+      (:h1 "Graffiti")
+      (:table
+       (loop for graffito in *graffiti*
+            do (cl-who:htm
+                (:tr
+                 (:td (cl-who:str (date graffito)))
+                 (:td (cl-who:str (ip graffito)))
+                 (:td (cl-who:str (message graffito)))))))
+      ))))
 
 (defvar *acceptor* nil
   "Hunchentoot acceptor.")
